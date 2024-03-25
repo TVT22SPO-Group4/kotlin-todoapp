78ba8115f6e99f1e28c9d4ac7b1e4e9262319328
laralaamanen <lara.laamanen@gmail.com>
2024-03-25 12:28:12 +0200

    create util and data classes

---
+++ app/src/main/java/com/example/to_doapp/util/Result.kt
@@ -0,0 +1,6 @@
+package com.example.to_doapp.util
+
+sealed class Result<out T : Any> {
+    data class Success<out T : Any>(val data: T) : Result<T>()
+    data class Failure(val exception: Exception) : Result<Nothing>()
+}
